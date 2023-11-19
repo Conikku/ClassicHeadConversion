ef3d1794f0fea8f5933db8822f7788360de04fd8
Conikku <72884174+Conikku@users.noreply.github.com>
2023-11-19 16:46:05 -0500

    Create FacesTable.lua
    Table for Heads

---
+++ FacesTable.lua
@@ -0,0 +1,493 @@
+--[[
+
+	Author: @Conikku
+	Start Date: August 17, 2023
+	Last Updated: November 17, 2023
+	
+	Note:
+	List of faces for ConversionHandler inside of a module so that way ConversionHandler isn't too lengthy
+	
+]]
+
+local FacesTable = {
+	
+	------------> Roblox Created Heads <------------
+	
+	-- Stevie/Default Smile
+	[13976201768] = { -- SpecialMesh
+		Face = "144080495",
+		Head = "5591363797",
+	},
+	[14380344401] = { -- MeshPart
+		Face = "144080495",
+		Head = "5591363797",
+	},
+
+	-- Woman
+	[12726021129] = { -- SpecialMesh
+		Face = "83022608",
+		Head = "8635368421",
+	},
+	[12995139274] = { -- MeshPart
+		Face = "83022608",
+		Head = "8635368421",
+	},
+
+	-- Man Face
+	[12733073232] = { -- SpecialMesh
+		Face = "83017053",
+		Head = "8635368147",
+	},
+	[12994951598] = { -- MeshPart
+		Face = "83017053",
+		Head = "8635368147",
+	},
+
+	-- Classic Female
+	[12732384881] = { -- SpecialMesh
+		Face = "3994345447",
+		Head = "8635368421",
+	},
+	[12732440328] = { -- MeshPart
+		Face = "3994345447",
+		Head = "8635368421",
+	},
+
+	-- Winning Smile
+	[12930419371] = { -- SpecialMesh
+		Face = "616395480",
+		Head = "5591363797",
+	},
+	[14377306239] = { -- MeshPart
+		Face = "616395480",
+		Head = "5591363797",
+	},
+
+	-- Classic Male
+	[12859107436] = { -- SpecialMesh
+		Face = "3994344171",
+		Head = "5591363797",
+	},
+	[12859102530] = { -- MeshPart
+		Face = "3994344171",
+		Head = "5591363797",
+	},
+
+	-- Chill
+	[12938732889] = { -- SpecialMesh
+		Face = "7074749",
+		Head = "5591363797",
+	},
+	[14521518488] = { -- MeshPart
+		Face = "7074749",
+		Head = "5591363797",
+	},
+
+	--Freckles
+	[12788914510] = { -- SpecialMesh
+		Face = "12145059",
+		Head = "8635368421",
+	},
+	[14918993907] = { -- MeshPart
+		Face = "12145059",
+		Head = "8635368421",
+	},
+
+	-- Squiggle
+	[12788528296] = { -- SpecialMesh
+		Face = "25165947",
+		Head = "8635368421",
+	},
+	[14401304993] = { -- MeshPart
+		Face = "25165947",
+		Head = "8635368421",
+	},
+
+	-- Err
+	[12859980784] = { -- SpecialMesh
+		Face = "20418518",
+		Head = "5591363797",
+	},
+	[12860895688] = { -- MeshPart
+		Face = "20418518",
+		Head = "5591363797",
+	},
+
+	--Check It
+	[11308920969] = { -- SpecialMesh
+		Face = "7074780",
+		Head = "5591363797",
+	},
+	[11445572546] = { -- MeshPart
+		Face = "7074780",
+		Head = "5591363797",
+	},
+
+	-- Stay Positive
+	[14719428401] = { -- SpecialMesh & MeshPart
+		Face = "209713952",
+		Head = "8635368421",
+	},
+
+	-- Cat Eye
+	[12936580274] = { -- SpecialMesh
+		Face = "7893435035",
+		Head = "8635368421",
+	},
+	[12936561806] = { -- MeshPart
+		Face = "7893435035",
+		Head = "8635368421",
+	},
+
+	-- Big Grin
+	[12936896604] = { -- SpecialMesh
+		Face = "7987146198",
+		Head = "8635368421",
+	},
+	[12936887700] = { -- MeshPart
+		Face = "7987146198",
+		Head = "8635368421",
+	},
+
+	-- :]
+	[12937029385] = { -- SpecialMesh
+		Face = "18151722",
+		Head = "5591363797",
+	},
+	[12937016609] = { -- MeshPart
+		Face = "18151722",
+		Head = "5591363797",
+	},
+
+	-- Laughing Fun
+	[12937157169] = { -- SpecialMesh
+		Face = "226216895",
+		Head = "8635368421",
+	},
+	[12937135097] = { -- MeshPart
+		Face = "226216895",
+		Head = "8635368421",
+	},
+
+	-- Know It All Grin
+	[12939027752] = { -- SpecialMesh
+		Face = "26424652",
+		Head = "5591363797",
+	},
+	[12939014597] = { -- MeshPart
+		Face = "26424652",
+		Head = "5591363797",
+	},
+
+	-- Braces
+	[12945371172] = { -- SpecialMesh
+		Face = "30394483",
+		Head = "5591363797",
+	},
+	[12945364777] = { -- MeshPart
+		Face = "30394483",
+		Head = "5591363797",
+	},
+
+	-- Yawn
+	[12945471762] = { -- SpecialMesh
+		Face = "161124757",
+		Head = "5591363797",
+	},
+	[12945456452] = { -- MeshPart
+		Face = "161124757",
+		Head = "5591363797",
+	},
+
+	-- Joyful
+	[12954195093] = { -- SpecialMesh
+		Face = "209713384",
+		Head = "5591363797",
+	},
+	[14374800823] = { -- MeshPart
+		Face = "209713384",
+		Head = "5591363797",
+	},
+
+	-- Whistle
+	[12991510818] = { -- SpecialMesh
+		Face = "22877700",
+		Head = "8635368421",
+	},
+	[14367118483] = { -- MeshPart
+		Face = "22877700",
+		Head = "8635368421",
+	},
+
+	-- Drool
+	[13674780763] = { -- SpecialMesh & Meshpart
+		Face = "7074882",
+		Head = "5591363797",
+	},
+
+	-- Hold it in
+	[2222767231] = { -- SpecialMesh & Meshpart
+		Face = "2222767231",
+		Head = "5591363797",
+	},
+	
+	-- Golden Eyes
+	[13675471354] = { -- SpecialMesh
+		Face = "9156248239",
+		Head = "8635368421",
+	},
+	
+	-- :D
+	[14637766494] = { -- SpecialMesh & Meshpart
+		Face = "406035320",
+		Head = "5591363797",
+	},
+	
+	-- Dizzy
+	[14695685417] = { -- SpecialMesh & Meshpart
+		Face = "10749463",
+		Head = "5591363797",
+	},
+	
+	-- Vampire
+	[13675435245] = { -- SpecialMesh & Meshpart
+		Face = "29532362",
+		Head = "5591363797",
+	},
+	
+	-- Heeeeeey...
+	[14638202807] = { -- SpecialMesh & Meshpart
+		Face = "21635489",
+		Head = "5591363797",
+	},
+	
+	-- Smil Nas X
+	[13684039612] = { -- SpecialMesh
+		Face = "5924588534",
+		Head = "8635368421",
+	},
+	
+	-- Hut Hut Hike!
+	[13684039612] = { -- SpecialMesh
+		Face = "15470849",
+		Head = "8635368421",
+	},
+	
+	-- Stare
+	[14638202807] = { -- SpecialMesh & Meshpart
+		Face = "8560915",
+		Head = "5591363797",
+	},
+	
+	-- Suspicious (like among us!?... I'll see myself out)
+	[14638202807] = { -- SpecialMesh & Meshpart
+		Face = "209715003",
+		Head = "5591363797",
+	},
+	
+	-- Friendly Smile
+	[13691523700] = { -- SpecialMesh & Meshpart
+		Face = "616394568",
+		Head = "5591363797",
+	},
+	
+	-- Grr!
+	[14769032055] = { -- SpecialMesh & Meshpart
+		Face = "19398553",
+		Head = "5591363797",
+	},
+	
+	-- Puck!
+	[14769032055] = { -- SpecialMesh & Meshpart
+		Face = "20298933",
+		Head = "5591363797",
+	},
+	
+	-- Finn McCool (insert WOOFER137 joke here)
+	[13692882504] = { -- SpecialMesh & Meshpart
+		Face = "10749456",
+		Head = "5591363797",
+	},
+	
+	-- Anime Surprise
+	[13692882504] = { -- SpecialMesh
+		Face = "416829065",
+		Head = "8635368421",
+	},
+	
+	-- Butterfly Wink
+	[14182901840] = { -- SpecialMesh
+		Face = "7657640018",
+		Head = "8635368421",
+	},
+	
+	-- Winky
+	[14719365169] = { -- SpecialMesh & Meshpart
+		Face = "7074856",
+		Head = "5591363797",
+	},
+	
+	-- Slick Fang
+	[14754580931] = { -- SpecialMesh & Meshpart
+		Face = "7317591",
+		Head = "5591363797",
+	},
+	
+	-- Glittering Eye
+	[13692882504] = { -- SpecialMesh
+		Face = "7893438453",
+		Head = "8635368421",
+	},
+	
+	-- Disbelief
+	[14755068153] = { -- SpecialMesh & Meshpart
+		Face = "20337265",
+		Head = "5591363797",
+	},
+	
+	-- Headless
+	[14760215320] = { -- SpecialMesh & Meshpart
+		Face = "13066630174",
+		Head = "6686307858", -- so, no head?
+	},
+	
+	-- Stitchface
+	[14760215320] = { -- SpecialMesh & Meshpart
+		Face = "8329438",
+		Head = "13916684351", -- so, new head?
+	},
+	
+	-- It's Go Time!
+	[14524485191] = { -- SpecialMesh & Meshpart
+		Face = "7506025",
+		Head = "5591363797",
+	},
+	
+	-- Cutie Mouse
+	[14525129330] = { -- SpecialMesh
+		Face = "15885042",
+		Head = "8635368421",
+	},
+	
+	-- Skeptic
+	[13001896404] = { -- SpecialMesh & Meshpart
+		Face = "31117192",
+		Head = "5591363797",
+	},
+
+	-- :3 (Hai! x3)
+	[14719225657] = { -- SpecialMesh & Meshpart
+		Face = "2222767231",
+		Head = "5591363797",
+	},
+	
+	-- So Funny
+	[14640105786] = { -- SpecialMesh & Meshpart
+		Face = "32058103",
+		Head = "5591363797",
+	},
+	
+	-- Not sure if
+	[13820802344] = { -- SpecialMesh & Meshpart
+		Face = "168332209",
+		Head = "5591363797",
+	},
+	
+	-- ROAR!!!
+	[14638466361] = { -- SpecialMesh & Meshpart
+		Face = "405704879",
+		Head = "13916684351",
+	},
+	
+	-- -_-
+	[13821294344] = { -- SpecialMesh & Meshpart
+		Face = "15637705",
+		Head = "5591363797",
+	},
+	
+	-- Joyous Surprise
+	[14840473015] = { -- SpecialMesh & Meshpart
+		Face = "28999175",
+		Head = "5591363797",
+	},
+	
+	-- Huh? 
+	[13822584343] = { -- SpecialMesh & Meshpart
+		Face = "150070505",
+		Head = "5591363797",
+	},
+	
+	-- Sinister
+	[14701679873] = { -- SpecialMesh & Meshpart
+		Face = "8329434",
+		Head = "5591363797",
+	},
+	
+	-- Red Lip
+	[13822985393] = { -- SpecialMesh
+		Face = "9650705589",
+		Head = "8635368421",
+	},
+	
+	-- I <3 New Site Theme (tmw when new site theme is just shades of the same color)
+	[13823226706] = { -- SpecialMesh & Meshpart
+		Face = "32723156",
+		Head = "5591363797",
+	},
+	
+	-- >_<
+	[14719266373] = { -- SpecialMesh & Meshpart
+		Face = "15324447",
+		Head = "5591363797",
+	},
+	
+	-- Ghost Face
+	[14192299483] = { -- SpecialMesh & Meshpart
+		Face = "12466911",
+		Head = "5591363797",
+	},
+	
+	-- Sniffles
+	[14192630651] = { -- SpecialMesh
+		Face = "14516479",
+		Head = "8635368421",
+	},
+	
+	-- I Hate Noobs
+	[14192299483] = { -- SpecialMesh & Meshpart
+		Face = "14030506",
+		Head = "5591363797",
+	},
+	
+	-- Big Sad Eyes
+	[14192863652] = { -- SpecialMesh & Meshpart
+		Face = "629933140",
+		Head = "5591363797",
+	},
+	
+	-- :-/
+	[14192993380] = { -- SpecialMesh & Meshpart
+		Face = "7075459",
+		Head = "5591363797",
+	},
+	
+	--  ^_^
+	[14193224583] = { -- SpecialMesh & Meshpart
+		Face = "15365479",
+		Head = "5591363797",
+	},
+	
+	-- Monster Face
+	[14205130925] = { -- SpecialMesh & Meshpart
+		Face = "49045252",
+		Head = "5591363797",
+	},
+	
+	-- :-O
+	[14205179825] = { -- SpecialMesh & Meshpart
+		Face = "7317691",
+		Head = "5591363797",
+	},
+}
+
+return FacesTable
